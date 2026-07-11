3710d6a8e38bdffd512b467a82918f7b495a1f47
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-11 12:07:55 +0800

    post create review

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/CreateReview.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 6: POST Create Review
@@ -17,6 +22,32 @@ public static class CreateReview
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
+        var reviewData = new
+        {
+            title = "Great Pasta!",
+            body = "Loved this recipe",
+            userId = 1
+        };
+
+        string jsonRequestBody = JsonSerializer.Serialize(reviewData);
+
+        using HttpContent content = new StringContent(jsonRequestBody, Encoding.UTF8, "application/json");
+        HttpResponseMessage response = await client.PostAsync("https://jsonplaceholder.typicode.com/posts", new StringContent("{\"title\": \"Great Pasta!\", \"body\": \"Loved this recipe\", \"userId\": 1}", System.Text.Encoding.UTF8, "application/json"));
+       
+        Debug.Assert(response.StatusCode == HttpStatusCode.Created, $"Expected HTTP 201 Created, but got {response.StatusCode}");
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+        JsonElement root = document.RootElement;
+
+        bool hasIdField = root.TryGetProperty("id", out JsonElement idElement);
+
+        Debug.Assert(hasIdField, "Expected response JSON to contain an 'id' field.");
+
+        int generatedId = idElement.GetInt32();
+        Debug.Assert(generatedId > 0, $"Expected a valid generated ID, but got {generatedId}");
+
         // TODO: Create JSON string with title, body, and userId
         // TODO: Create StringContent with the JSON and Content-Type "application/json"
         // TODO: Send POST request to https://jsonplaceholder.typicode.com/posts
@@ -24,6 +55,5 @@ public static class CreateReview
         // TODO: Parse the response JSON
         // TODO: Assert the response has an "id" field with a value
 
-        throw new NotImplementedException();
     }
 }
