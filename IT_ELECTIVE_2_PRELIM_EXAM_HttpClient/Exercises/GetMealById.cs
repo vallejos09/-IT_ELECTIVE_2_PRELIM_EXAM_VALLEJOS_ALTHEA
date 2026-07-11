7ea1e68fdef5c01ff960ca6e0b01cc86c059b672
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-11 11:56:15 +0800

    get meal by id

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 3: GET Lookup by ID
@@ -14,11 +18,23 @@ public static class GetMealById
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
+        HttpResponseMessage response = await client.GetAsync("https://themealdb.com/api/json/v1/1/lookup.php?i=52771");
+        Debug.Assert(response.StatusCode == HttpStatusCode.OK, $"Expected HTTP 200 OK, but got {response.StatusCode}");
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+        JsonElement root = document.RootElement;
+        JsonElement mealsArray = root.GetProperty("meals");
+
+        string? mealName = mealsArray[0].GetProperty("strMeal").GetString();
+
+        Debug.Assert(mealName != null && mealName.Contains("Arrabiata"), $"Expected meal name to contain 'Arrabiata', but got '{mealName}'");
+
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=52771
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert the meal name (strMeal) is "Arrabiata"
 
-        throw new NotImplementedException();
     }
 }
