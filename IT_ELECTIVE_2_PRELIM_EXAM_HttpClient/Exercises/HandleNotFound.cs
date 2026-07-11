67c81fdf5eac7df18a4f07c075e165d339d73450
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-11 12:15:19 +0800

    handle not found

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 9: GET Handle 404 Not Found
@@ -16,11 +19,23 @@ public static class HandleNotFound
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
+        HttpResponseMessage response = await client.GetAsync("https://themealdb.com/api/json/v1/1/lookup.php?i=999999");
+        Debug.Assert(response.StatusCode == System.Net.HttpStatusCode.OK, $"Expected HTTP 200 OK, but got {response.StatusCode}");
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+        JsonElement root = document.RootElement;
+        JsonElement mealsElement = root.GetProperty("meals");
+
+        bool isMealsNull = mealsElement.ValueKind == JsonValueKind.Null;
+
+        Debug.Assert(isMealsNull, "Expected the 'meals' property to be null for an invalid ID lookup.");
+
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=999999
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert that "meals" field is null (not found)
 
-        throw new NotImplementedException();
     }
 }
