48239d36a1f682e8e1872d843fa1402f63502d95
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-11 11:44:04 +0800

    search meal by name

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/SearchMealByName.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/SearchMealByName.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 2: GET Search by Name
@@ -15,11 +18,24 @@ public static class SearchMealByName
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
+
+        HttpResponseMessage response =
+            await client.GetAsync("https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+
+        Debug.Assert(response.StatusCode == System.Net.HttpStatusCode.OK);
+
+        String json = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument document = JsonDocument.Parse(json);
+
+        JsonElement meals = document.RootElement.GetProperty("meals");
+
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert that the "meals" array is not null and has at least 1 item
 
-        throw new NotImplementedException();
+        Debug.Assert(meals.ValueKind == JsonValueKind.Array);
+        Debug.Assert(meals.GetArrayLength() >= 1);
     }
 }
