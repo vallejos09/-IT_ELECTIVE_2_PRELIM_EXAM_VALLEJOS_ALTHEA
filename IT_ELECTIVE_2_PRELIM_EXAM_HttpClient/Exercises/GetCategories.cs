5df372e7db59ad858edbed9a02ef3c1ef21789c9
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-11 11:59:08 +0800

    get categories

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetCategories.cs
@@ -1,3 +1,7 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 4: GET List Categories
@@ -14,11 +18,23 @@ public static class GetCategories
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
+        HttpResponseMessage response = await client.GetAsync("https://themealdb.com/api/json/v1/1/categories.php");
+        Debug.Assert(response.StatusCode == HttpStatusCode.OK, $"Expected HTTP 200 OK, but got {response.StatusCode}");
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+        JsonElement root = document.RootElement;
+        JsonElement categoriesArray = root.GetProperty("categories");
+
+        int categoryCount = categoriesArray.GetArrayLength();
+
+        Debug.Assert(categoryCount > 0, "Expected categories array to have more than 0 items, but it was empty.");
+
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/categories.php
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert the "categories" array has more than 0 items
 
-        throw new NotImplementedException();
     }
 }
