40f39b3921f3816bc2f6efa85a116929e3efdea7
Althea Vallejos <teyakuromi.99@gmail.com>
2026-07-11 12:02:06 +0800

    filter by ingredient

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/FilterByIngredient.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/FilterByIngredient.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 5: GET Filter by Ingredient
@@ -14,11 +17,23 @@ public static class FilterByIngredient
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
+        HttpResponseMessage response = await client.GetAsync("https://themealdb.com/api/json/v1/1/filter.php?i=chicken_breast");
+        Debug.Assert(response.StatusCode == System.Net.HttpStatusCode.OK, $"Expected HTTP 200 OK, but got {response.StatusCode}");
+
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+        JsonElement root = document.RootElement;
+        JsonElement mealsArray = root.GetProperty("meals");
+
+        int mealCount = mealsArray.GetArrayLength();
+
+        Debug.Assert(mealCount >= 1, $"Expected at least 1 meal, but the array length was {mealCount}.");
+
         // TODO: Send GET request to https://themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
         // TODO: Assert status code is 200 OK
         // TODO: Parse the response JSON
         // TODO: Assert the "meals" array has at least 1 item
 
-        throw new NotImplementedException();
     }
 }
